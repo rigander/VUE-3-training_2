0209ae9fd9e2a4b7d92b30779c4825cb185bc5e6
Andrey Komov <andrewkomov@gmail.com>
2022-06-24 00:58:56 +0300

    Animation. <transition group>

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
 <div v-if="posts.length > 0 ">
   <h3>List of posts</h3>
-  <PostItem
-      v-for="post in posts"
-      :post="post"
-      :key="post.id"
-      @remove="$emit('remove', post)"
-  />
+  <transition-group name="post-list">
+    <PostItem
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+    />
+  </transition-group>
 </div>
   <h2 v-else style="color: rgba(255,166,0,0.74)">
     List of posts is empty!
@@ -28,5 +30,18 @@ export default {
 </script>
 
 <style scoped>
-
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active, .post-list-leave-active {
+  transition: all 0.4s;
+}
+.post-list-enter, .post-list-leave-to /* .post-list-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform 0.4s ease;
+}
 </style>
