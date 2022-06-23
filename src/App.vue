df340e075da147693aaafc24a3b960a56e11ea72
Andrey Komov <andrewkomov@gmail.com>
2022-06-24 00:05:27 +0300

    Computed. Sort posts via select option choise

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
       />
     </my-dialog>
       <PostList
-         :posts="posts"
+         :posts="sortedPosts"
          @remove="removePost"
          v-if="!isPostsLoading"
       />
@@ -80,14 +80,13 @@ export default {
   mounted() {
     this.fetchPosts()
   },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+    }
+  },
   watch: {
-    selectedSort(newValue) {
-      this.posts.sort( (post1, post2) => {
-        return post1[newValue]?.localeCompare(post2[newValue])
-      })
     },
-  }
-
 }
 </script>
 
