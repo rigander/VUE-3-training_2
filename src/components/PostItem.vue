adc33e8bc005c4cb12611c765f5d48021e2f239d
Andrey Komov <andrewkomov@gmail.com>
2022-06-25 18:39:39 +0300

    dynamic navigation

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="post">
-    <div>
-      <div>{{ post.id }}</div>
+    <div class="post__text">
+      <div class="post__id">{{ post.id }}</div>
       <div><strong>Name : </strong>{{ post.title }}</div>
       <div class="post-body"><strong>Description : </strong>{{ post.body }}</div>
     </div>
     <div class="post__btns">
+      <my-button
+          @click="$router.push(`/pages/${post.id}`)"
+      >Open Post
+      </my-button>
       <my-button
        @click="$emit('remove', post)"
       >Delete Post
@@ -31,14 +35,24 @@ export default {
 <style scoped>
 .post {
   border: 2px solid cadetblue;
-  border-radius: 1px;
+  border-radius: 3px;
   margin-top: 15px;
   padding: 15px;
   display: flex;
   align-items: center;
   justify-content: space-between;
 }
-.post-body{
-  padding-right: 220px;
+.post__btns{
+  height: 70px;
+  width: 130px;
+  margin-bottom: 20px;
+}
+.post__text{
+  width: 740px;
+}
+.post__id{
+  color:green;
+  font-size: 24px;
+  font-weight: bold;
 }
 </style>
---
+++ src/pages/PostIdPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>It's a post page with id = {{ $route.params.id }}</h1>
+</template>
+
+<script>
+export default {
+  name: "PostIdPage"
+}
+</script>
+
+<style scoped>
+
+</style>
